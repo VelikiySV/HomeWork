557620e892aa7abb0e9a583b721b970ecd5f5731
Volodymyr Silitskyi <Volodymyr_Silitskyi@epam.com>
2016-03-30 12:17:36 +0300

    Initial commit

---
+++ src/MyFirstClass.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Volodymyr_Silitskyi on 3/30/2016.
+ */
+public class MyFirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
