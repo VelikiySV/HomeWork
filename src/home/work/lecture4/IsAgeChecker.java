8272798fec6fb7580c88314cfdc88cd09d325612
Volodymyr Silitskyi <Volodymyr_Silitskyi@epam.com>
2016-04-12 12:06:03 +0300

    HomeWork L4

---
+++ src/home/work/lecture4/IsAgeChecker.java
@@ -0,0 +1,16 @@
+package home.work.lecture4;
+
+/**
+ * Created by Volodymyr_Silitskyi on 4/12/2016.
+ */
+public class IsAgeChecker {
+    public static void main(String[] args) {
+        //Input
+        int age = 1;
+
+        boolean isAge = (age >= 1) && (<= 120);
+
+        if (isAge) {
+            System.out.println("Age is ");
+    }
+}
---
+++ src/home/work/lecture4/MinMaxCalculator.java
@@ -0,0 +1,19 @@
+package home.work.lecture4;
+
+/**
+ * Created by Volodymyr_Silitskyi on 4/12/2016.
+ */
+public class MinMaxCalculator {
+    public static void main(String[] args) {
+        // input
+        int a = 9;
+        int b = 6;
+
+        if (a > b){
+            System.out.println("Max is " + a);
+        }
+    else{
+            System.out.println("Min is " + b);
+        }
+    }
+}
