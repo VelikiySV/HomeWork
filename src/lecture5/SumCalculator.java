50c344464106c8ef0d3176e6b6051e67a4f86854
Volodymyr Silitskyi <Volodymyr_Silitskyi@epam.com>
2016-04-13 13:22:09 +0300

    Lecture5

--- src/lecture5/SumCalculator.java
+++ src/lecture5/SumCalculator.java
@@ -8,8 +8,22 @@ import java.util.Scanner;
 public class SumCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int sum = 9;
+        int n;
+        do {
+            System.out.println("Please enter number ");
+            n = scanner.nextInt();
+
+            sum += n;
+        } while (n != 0);
 
 
+    System.out.println("sum = "+sum);
+}
+//        if (i != 0) {
+//            System.out.println("Please enter some number ");
+//            String sum = scanner.next();
 
-    }
+
 }
