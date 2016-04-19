704d5de27d1418be50270353ce0bfa350eb80738
Volodymyr Silitskyi <Volodymyr_Silitskyi@epam.com>
2016-04-19 17:03:14 +0300

    Home work 6 "Array"

--- src/home/work/lecture6/ArrayPositiveFinder.java
+++ src/home/work/lecture6/ArrayPositiveFinder.java
@@ -5,27 +5,40 @@ package home.work.lecture6;
  */
 public class ArrayPositiveFinder {
     public static void main(String[] args) {
-        int [] array = new int[] {-30, -21, -2, 2, -50, 1, -2, -3,};
-        findFirstPositive(array);
-        findLastPositive(array);
+
+        int [] array = new int[] {-30, -21, -2, 2, -50, -1, -2, 3};
+        int firstPositive = findFirstPositive(array);
+
+        if (firstPositive == 0){
+            System.out.println(-1);
+        }
+        else{
+            findLastPositive(array);
+        }
+
+    }
+
+
+    private static int findFirstPositive(int[] array) {
+        int first = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 0) {
+                first = array[i];
+                System.out.println("First positive value is " + first);
+                break;
+            }
+        }
+        return first;
     }
 
     private static void findLastPositive(int[] array) {
+        int last = 0;
         for (int i = array.length - 1; i >= 0; i--) {
             if (array[i] > 0) {
-                System.out.println("Last positive value is " + array[i]);
+                last = array[i];
+                System.out.println("Last positive value is " + last);
                 break;
             }
         }
     }
-
-    private static void findFirstPositive(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] > 0) {
-                System.out.println("First positive value is " + array[i]);
-                break;
-            }
-        }
-    }
-
 }
