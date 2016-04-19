933e641426ee3d0d1a95958fc6f5bb45ef5184df
Volodymyr Silitskyi <Volodymyr_Silitskyi@epam.com>
2016-04-19 12:04:04 +0300

    Lecture5

--- src/Lacture6/BlackBox.java
+++ src/Lacture6/BlackBox.java
@@ -1,21 +1,24 @@
-//package Lacture6;
-//
-///**
-// * Created by Volodymyr_Silitskyi on 4/14/2016.
-// */
-//public class BlackBox {
-//    public static void main(String[] args) {
+package Lacture6;
+
+/**
+ * Created by Volodymyr_Silitskyi on 4/14/2016.
+ */
+public class BlackBox {
+    public static void main(String[] args) {
 //        //input
-//        int from = 1;
-//        int to = 1000;
-//
-//        //BL
-//        public static int rangeSum(int from, int to){
-//        int sum = 0;
-//        for (int i = from; i <= to; i++) {
-//            sum += i;
-//        }
-//        return sum;
-//    }
-//
-//}
+        int from = 1;
+        int to = 100;
+
+        int rangeSum = rangeSum(from, to);
+        System.out.println(rangeSum);
+    }
+        //BL
+
+        public static int rangeSum(int from, int to) {
+            int rangeSum = 0;
+            for (int i = from; i <= to; i++) {
+                rangeSum += i;
+         }
+         return rangeSum;
+     }
+}
--- src/home/work/lecture4/MinMaxCalculator.java
+++ src/home/work/lecture4/MinMaxCalculator.java
@@ -3,19 +3,19 @@ package home.work.lecture4;
 /**
  * Created by Volodymyr_Silitskyi on 4/12/2016.
  */
-//public class MinMaxCalculator {
-//        public static void main(String[] args) {
-////        // input
-////        int a = 9;
-////        int b = 6;
-////
-////        if (a > b){
-////            System.out.println("Max is " + a);
-////        }
-////    else{
-////            System.out.println("Min is " + b);
-////        }
-////    }
+public class MinMaxCalculator {
+        public static void main(String[] args) {
+        // input
+        int a = 9;
+        int b = 6;
+
+        if (a > b){
+            System.out.println("Max is " + a);
+        }
+    else{
+            System.out.println("Min is " + b);
+        }
+    }
 //
 ////    public static void main(String[] args) {
 //        int a = 4;
@@ -41,6 +41,6 @@ package home.work.lecture4;
 //
 //
 //}
-//}
+}
 
 
