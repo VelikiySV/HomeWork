cd3cd2dc84be395838a04b910fdceb129484672b
Volodymyr Silitskyi <Volodymyr_Silitskyi@epam.com>
2016-04-20 10:18:52 +0300

    Lecture7"Array"

---
+++ src/Lecture7/TrianglePrinter.java
@@ -0,0 +1,22 @@
+//package Lecture7;
+//
+///**
+// * Created by Volodymyr_Silitskyi on 4/19/2016.
+// */
+//public class TrianglePrinter {
+//    public static void main(String[] args) {
+//        checkSize();
+//
+//        for (int rowSize = 0; rowSize < size ; rowSize++) {
+//            printRow(rowSize);
+//        }
+//    }
+//
+//    private static void checkSize() {
+//        if (size < 0){
+//            throw new IllegalArgumentException("Incorrect size");
+//        }
+//    }
+//
+//}
+
